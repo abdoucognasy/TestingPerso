6cf6875123545d7696208a46c3b3999b7643156c
Abdou Cogna Sy <abdou-cogna.sy-prestataire@laposte.fr>
2021-12-02 00:41:19 +0000

    Ajoutez des fichiers projet.

---
+++ testingDevoir.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+
+namespace TestingPerso
+{
+    public class testingDevoir : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            IPluginExecutionContext context = serviceProvider.GetService(typeof(IPluginExecutionContext)) as IPluginExecutionContext;
+            ITracingService tracingService = serviceProvider.GetService(typeof(ITracingService)) as ITracingService;
+            IOrganizationServiceFactory serviceFactory = serviceProvider.GetService(typeof(ITracingService)) as IOrganizationServiceFactory;
+            IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+            try
+            {
+                switch (context.MessageName)
+                {
+                    case "create":
+                        break;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
+        }
+    }
+}
